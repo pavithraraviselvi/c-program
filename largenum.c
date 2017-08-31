cca071c684bcac99b14bc5826649d7a729af9cff
pavithraraviselvi <31505530+pavithraraviselvi@users.noreply.github.com>
2017-08-31 15:18:04 +0530

    Create largenum.c

---
+++ largenum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter two numbers");
+scanf("%d%d"a,b);
+if(a>b)
+{
+printf("a is greater");
+}
+else
+{
+printf("b is greater");
+}
+return0;
+}
